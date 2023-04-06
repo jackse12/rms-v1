a64f517dbbd01b5cfe9f26f2d51ac08c4eb58b6c
MBSFMS <matprog217@gmail.com>
2023-04-06 17:15:45 +0800

    add auth on dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import FeatureFunc from "@/component/familyChartFunc"
+import withAuth from '@/hoc/withAuth'
 function FeatureFuncIndex() {
   return (
     <div><FeatureFunc /></div>
   )
 }
 
-export default FeatureFuncIndex
+export default withAuth(FeatureFuncIndex) 
